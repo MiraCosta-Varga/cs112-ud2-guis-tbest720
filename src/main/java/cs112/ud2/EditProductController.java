b6913e5a997ce3a12cfa714c342e22515272c45c
SmonkLord <tbest1996@gmail.com>
2024-11-18 21:31:15 -0800

    Added All FXML Files, Java Classes, CSS StyleSheets, and project resources. All code is running.

---
+++ src/main/java/cs112/ud2/EditProductController.java
@@ -0,0 +1,14 @@
+package cs112.ud2;
+
+import javafx.fxml.FXML;
+
+public class EditProductController {
+    @FXML
+    public void searchProducts() {
+        System.out.println("Searching products");
+    }
+
+    public void editProduct() {
+        System.out.println("Editing product");
+    }
+}
---
+++ src/main/java/cs112/ud2/InvalidThreadCodeException.java
@@ -0,0 +1,8 @@
+package cs112.ud2;
+
+public class InvalidThreadCodeException extends Exception {
+    public InvalidThreadCodeException(String message) {
+        super(message);
+    }
+
+}
